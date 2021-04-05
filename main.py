bc8dd41e1171b75d9dcfb1ae1087b5f1cac7c774
Nazar Dzhura <nazar_dzhura@ukr.net>
2021-04-05 16:10:41 +0300

    Performance of program was improved by optimizing regular expressions.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import re
-import datetime
 
 if __name__ == "__main__":
     REQUEST_TYPE = "GET"
@@ -7,15 +6,7 @@ if __name__ == "__main__":
     REQUEST_STATUS_TO: int = 400
 
     counter_of_lines: int = 0
-    all_possible_points_in_time = []
 
-    start_time = datetime.datetime(1111, 1, 1, 2, 19, 30)
-    end_time = datetime.datetime(1111, 1, 1, 11, 9, 29)
-    delta = datetime.timedelta(seconds=1)
-
-    while start_time <= end_time:
-        all_possible_points_in_time.append(*re.findall(r'\d{2}:\d{2}:\d{2}', str(start_time)))
-        start_time += delta
     with open("access.log.txt", "r") as file:
         list_of_lines = file.readlines()
 
@@ -24,19 +15,53 @@ if __name__ == "__main__":
         is_requested_time = False
         is_request_type_suitable = False
 
+        # Checking if request type is suitable
         if f"\"{REQUEST_TYPE} " in line:
             is_request_type_suitable = True
 
+        # Checking if request is succeed
         for code in range(REQUEST_STATUS_FROM, REQUEST_STATUS_TO, 1):
             if f" {code} " in line:
                 is_request_success = True
                 break
 
-        for point_of_time in all_possible_points_in_time:
-            if f":{point_of_time} " in line:
-                is_requested_time = True
-                break
+        # Checking if request is in suitable time range
+        full_time = re.findall(r'\d{2}/\w{3}/\d{4}:(\d{2}:\d{2}:\d{2})', line)
+        is_hour_suitable = False
+        is_minute_suitable = False
+        is_second_suitable = False
 
+        if 2 <= int(full_time[0][:2]) <= 11:
+            is_hour_suitable = True
+
+        if 2 < int(full_time[0][:2]) < 11:
+            is_minute_suitable = True
+        elif int(full_time[0][:2]) == 2:
+            if int(full_time[0][3:5]) >= 19:
+                is_minute_suitable = True
+        elif int(full_time[0][:2]) == 11:
+            if int(full_time[0][3:5]) <= 9:
+                is_minute_suitable = True
+        else:
+            is_minute_suitable = False
+
+        if 2 < int(full_time[0][:2]) < 11:
+            is_second_suitable = True
+        elif int(full_time[0][:2]) == 2 and int(full_time[0][3:5]) > 19:
+            is_second_suitable = True
+        elif int(full_time[0][:2]) == 2 and int(full_time[0][3:5]) == 19:
+            if int(full_time[0][2:]) >= 30:
+                is_second_suitable = True
+        elif int(full_time[0][:2]) == 11 and int(full_time[0][3:5]) < 9:
+            is_second_suitable = True
+        elif int(full_time[0][:2]) == 11 and int(full_time[0][3:5]) == 9:
+            if int(full_time[0][2:]) <= 29:
+                is_second_suitable = True
+
+        if is_hour_suitable and is_minute_suitable and is_second_suitable:
+            is_requested_time = True
+
+        # Checking if all conditions are 'True'
         if is_request_type_suitable and is_request_success and is_requested_time:
             counter_of_lines += 1
 
